176d8de58c57f17b3f0f7c69db76aa3755eef7d0
accazeta <f.panzeri24@studenti.unibg.it>
2022-07-05 14:39:13 +0200

    Alcuni cambiamenti

--- functions.py
+++ functions.py
@@ -1,16 +1,56 @@
-def checkAvailableYears(ws):
+def check_available_years(ws):
     lista_anni = []
     for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
         if row[0].value not in lista_anni:
             lista_anni.append(row[0].value)
     return lista_anni
 
-def checkMissingYears(lista_anni):
+
+def check_missing_years(lista_anni):
     lista_anni.sort()
     gap_years = []
-
     year = lista_anni[0]
     while year != lista_anni[-1]:
-        if int(year) + 1 not in lista_anni:
-            gap_years.append(int(year) + 1)
+        if year + 1 not in lista_anni:
+            gap_years.append(year + 1)
         year = year + 1
+
+
+def somma_nuovi_nati(ws, nome):
+    somma = 0
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+        if row[1].value.lower() == nome.lower():
+            somma += row[4].value
+    return somma
+
+
+def somma_nuovi_nati(ws, anno):
+    somma = 0
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+        if row[0].value == anno:
+            somma += row[4].value
+    return somma
+
+
+def somma_nuovi_nati(ws, nazionalità):
+    somma = 0
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+        if row[3].value == nazionalità:
+            somma += row[4].value
+    return somma
+
+
+def check_by_name(ws, nome):
+    somma = somma_nuovi_nati(ws, nome)
+    print("In tutto ci sono {} persone di nome {}.".format(somma, nome))
+    print("Questa è la distribuzione nel corso degli anni: ")
+
+
+def check_by_year(ws, anno):
+    somma = somma_nuovi_nati(ws, anno)
+    print("In tutto nel {} sono nate {} persone.".format(anno, somma))
+
+
+def check_by_nazionalità(ws, nazionalità):
+    somma = somma_nuovi_nati(ws, nazionalità)
+    print("In tutto nel {} sono nate {} persone.".format(nazionalità, somma))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import functions as f
 import traceback
-from openpyxl import workbook, load_workbook, Workbook
+from openpyxl import load_workbook
+
+import functions as f
 
 try:
     wb = load_workbook('Nomi_Neonati_Bergamo.xlsx')
@@ -20,13 +21,37 @@ try:
 
     try:
         # nome = input("Inserisci il nome che vuoi cercare: ")
-        lista_anni = f.checkAvailableYears(ws)
-        gap_years = f.checkMissingYears(lista_anni)
+        lista_anni = f.check_available_years(ws)
+        gap_years = f.check_missing_years(lista_anni)
         if gap_years is None:
             print("Analizzo i dati dal {} al {}".format(lista_anni[0], lista_anni[-1]))
         else:
-            print("Analizzo i dati dal {} al {}, tuttavia mancano questi anni: {}".format(lista_anni[0], lista_anni[-1], gap_years))
+            print("Analizzo i dati dal {} al {}, tuttavia mancano questi anni: {}".format(lista_anni[0], lista_anni[-1],
+                                                                                          gap_years))
+
+        scelta = 999
+        while scelta != 0:
+            print(
+                "\nMenu: \n1) Ricerca per nome\n2) Ricerca per anno\n3) Ricerca per nazionalità\n4) Ricerca per "
+                "genere\n0) per chiudere")
+            scelta = int(input("Scelta: "))
+            match scelta:
+                case 1:
+                    nome = input("Inserisci il nome da cercare: ")
+                    f.check_by_name(ws, nome=nome)
+                # case 2:
+                # codice
+                # case 3:
+                # codice
+                # case 4:
+                # codice
+                # case 0:
+                # codice
+                case _:
+                    scelta = 999
     except:
         print(traceback.format_exc())
 except FileNotFoundError:
+    print("Errore, file non trovato")
+except:
     print(traceback.format_exc())
